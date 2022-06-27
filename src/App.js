3dec96cc38a5f223aac28f2a8b9cbca944e482bb
Lance Liu <lanceliumeng89@gmail.com>
2022-06-27 12:29:43 +1000

    remember clean up event listener in component in case add too much same event listener in browser

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import React from "react";
-import UseEffectBasics from "./tutorial/2-useEffect/setup/1-useEffect-basics";
+import UseEffectCleanup from "./tutorial/2-useEffect/setup/2-useEffect-cleanup";
 function App() {
   return (
     <div className="container">
-      <UseEffectBasics />
+      <UseEffectCleanup />
     </div>
   );
 }
--- src/tutorial/2-useEffect/setup/2-useEffect-cleanup.js
+++ src/tutorial/2-useEffect/setup/2-useEffect-cleanup.js
@@ -1,10 +1,32 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from "react";
 
 // cleanup function
 // second argument
 
 const UseEffectCleanup = () => {
-  return <h2>useEffect cleanup</h2>;
+  const [size, setSize] = useState(window.innerWidth);
+
+  const checkSize = () => {
+    setSize(window.innerWidth);
+  };
+
+  useEffect(() => {
+    console.log("use Effect");
+    window.addEventListener("resize", checkSize);
+    console.log(size);
+    //remeber: everytime when we addEventListener, we need to remove it incase we put too much same event listener in browser
+    return () => {
+      console.log("cleanup");
+      window.removeEventListener("resize", checkSize);
+    };
+  }, []);
+  console.log("render", size);
+  return (
+    <>
+      <h1>Window Size</h1>
+      <h2>{size} PX</h2>
+    </>
+  );
 };
 
 export default UseEffectCleanup;
