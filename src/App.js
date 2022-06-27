2774333f1152cff8ef240f343e809b460f31bcba
Lance Liu <lanceliumeng89@gmail.com>
2022-06-27 11:35:56 +1000

    complete useEffect basic pratice and leave comment note for review

--- src/App.js
+++ src/App.js
@@ -1,11 +1,9 @@
 import React from "react";
-// import Setup from "./tutorial/1-useState/setup/1-error-example";
-// import Final from "./tutorial/1-useState/final/2-useState-basics";
-import UseStateArray from "./tutorial/1-useState/setup/3-useState-array";
+import UseEffectBasics from "./tutorial/2-useEffect/setup/1-useEffect-basics";
 function App() {
   return (
     <div className="container">
-      <UseStateArray />
+      <UseEffectBasics />
     </div>
   );
 }
--- src/tutorial/2-useEffect/setup/1-useEffect-basics.js
+++ src/tutorial/2-useEffect/setup/1-useEffect-basics.js
@@ -1,9 +1,32 @@
-import React, { useState, useEffect } from 'react';
-// by default runs after every re-render
-// cleanup function
-// second parameter
+import React, { useState, useEffect } from "react";
+// Note:
+// => by default(no dependency []) runs after every re-render
+// => second parameter:
+//1. dependency is [], useEffect only run on the initial render
+//2. dependency is [xxx,xxx], useEffect only run on xxx changed
+// => just like useState, useEffect can be used as much times as we want in one component.
+
 const UseEffectBasics = () => {
-  return <h2>useEffect Basics</h2>;
+  const [count, SetCount] = useState(0);
+  useEffect(() => {
+    console.log("call useEffect");
+    if (count >= 1) {
+      document.title = `${count}`;
+    }
+  }, [count]);
+  useEffect(() => {
+    console.log("the second useEffect just call once");
+  }, []);
+  console.log("render component");
+  const btnClickHandler = () => SetCount((prevCount) => prevCount + 1);
+  return (
+    <>
+      <h2>{count}</h2>
+      <button type="button" className="btn" onClick={btnClickHandler}>
+        Click
+      </button>
+    </>
+  );
 };
 
 export default UseEffectBasics;
