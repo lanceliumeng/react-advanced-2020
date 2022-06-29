6009f1d6312eef91b2bc7a3d7235a44744d523c9
Lance Liu <lanceliumeng89@gmail.com>
2022-06-29 11:00:19 +1000

    update README and complere 04 forms first practice

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import React from "react";
-import ShowHide from "./tutorial/3-conditional-rendering/setup/3-show-hide";
+import ControlledInputs from "./tutorial/4-forms/setup/1-controlled-inputs";
 function App() {
   return (
     <div className="container">
-      <ShowHide />
+      <ControlledInputs />
     </div>
   );
 }
--- src/tutorial/4-forms/setup/1-controlled-inputs.js
+++ src/tutorial/4-forms/setup/1-controlled-inputs.js
@@ -1,12 +1,43 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 // JS
 // const input = document.getElementById('myText');
 // const inputValue = input.value
 // React
 // value, onChange
+//In this case, React form has two way to handle form submit
+// 1. submitHandler as below, + {/* <form className="form" onSubmit={submitHandler}> */}
+// OR
+// 2. submitHandler as below, +  {/* <button type="submit" onClick={submitHandler}>Add Person</button> */}
 
 const ControlledInputs = () => {
-  return <h1>controlled inputs</h1>;
+  const submitHandler = (event) => {
+    event.preventDefault();
+    console.log("Hello World");
+  };
+  return (
+    <>
+      <article>
+        {/* <form className="form" onSubmit={submitHandler}> */}
+        <form className="form" onSubmit={submitHandler}>
+          {/* React From nomarly has label and input */}
+          {/* label should has htmlFor attribute, input need to include id and name attributes */}
+          <div className="form-control">
+            <label htmlFor="firstname">Name :</label>
+            <input type="text" id="firstName" name="firstName" />
+          </div>
+
+          <div className="form-control">
+            <label htmlFor="email">Email :</label>
+            <input type="email" id="email" name="email" />
+          </div>
+          <button type="submit">Add Person</button>
+          {/* <button type="submit" onClick={submitHandler}>
+            Add Person
+          </button> */}
+        </form>
+      </article>
+    </>
+  );
 };
 
 export default ControlledInputs;
