31cc6c29c428d10923c5441c21877339d7d2c0dc
Lance Liu <lanceliumeng89@gmail.com>
2022-06-29 11:36:14 +1000

    complete form inputs value and states, also can take the new state value into UI

--- src/tutorial/4-forms/setup/1-controlled-inputs.js
+++ src/tutorial/4-forms/setup/1-controlled-inputs.js
@@ -9,10 +9,31 @@ import React, { useState } from "react";
 // OR
 // 2. submitHandler as below, +  {/* <button type="submit" onClick={submitHandler}>Add Person</button> */}
 
+//another point is when we use state into form input value attribute
+//we also need to add onChange event handler for set state update.
+
 const ControlledInputs = () => {
+  const [firstName, setFirstName] = useState("");
+  const [email, setEmail] = useState("");
+  const [people, setPeople] = useState([]);
+
   const submitHandler = (event) => {
     event.preventDefault();
-    console.log("Hello World");
+    // console.log(firstName, email);
+    if (firstName && email) {
+      // const person = { firstName: firstName, email: email };
+      // above one is ok but in ES6 we have shorthand:
+      const person = { id: new Date().getTime().toString(), firstName, email };
+      //I still perfer use callback and prevState and then add new data into old state
+      //I think this way is more safer
+      setPeople((prevState) => {
+        return [...prevState, person];
+      });
+      setFirstName("");
+      setEmail("");
+    } else {
+      console.log("empty values!");
+    }
   };
   return (
     <>
@@ -23,18 +44,39 @@ const ControlledInputs = () => {
           {/* label should has htmlFor attribute, input need to include id and name attributes */}
           <div className="form-control">
             <label htmlFor="firstname">Name :</label>
-            <input type="text" id="firstName" name="firstName" />
+            <input
+              type="text"
+              id="firstName"
+              name="firstName"
+              value={firstName}
+              onChange={(e) => setFirstName(e.target.value)}
+            />
           </div>
 
           <div className="form-control">
             <label htmlFor="email">Email :</label>
-            <input type="email" id="email" name="email" />
+            <input
+              type="email"
+              id="email"
+              name="email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
           </div>
           <button type="submit">Add Person</button>
           {/* <button type="submit" onClick={submitHandler}>
             Add Person
           </button> */}
         </form>
+        {people.map((person) => {
+          const { id, firstName, email } = person;
+          return (
+            <div key={id} className="item">
+              <h4>{firstName}</h4>
+              <p>{email}</p>
+            </div>
+          );
+        })}
       </article>
     </>
   );
